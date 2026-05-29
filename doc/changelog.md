dd4e027d2ab6da6b9d5a92024262ff6872cdfea5
CI Build <ci@ushell.org>
2026-05-29 09:24:13 +0000

    deleted orphaned file -> VERSIONING [skip ci]

--- doc/changelog.md
+++ doc/changelog.md
@@ -12,7 +12,13 @@ This files contains a version history including all changes relevant for semanti
 
 ## Upcoming Changes
 
-* **new Feature**: Moved targets from .net 6/8 to 8/10
+*(none)*
+
+
+
+## v 2.1.0
+released **2026-05-29**, including:
+ - **new Feature**: Moved targets from .net 6/8 to 8/10
 
 
 
--- dotnet/src/ModelDescription.net48/Properties/AssemblyInfo.cs
+++ dotnet/src/ModelDescription.net48/Properties/AssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("46adc8b8-2f14-4a31-87ca-95b71ee09ea9")]
 
-[assembly: AssemblyVersion("2.0.22")]
-[assembly: AssemblyInformationalVersion("2.0.22")]
-//DONT ADD: [assembly: AssemblyFileVersion("2.0.22")] !!!
+[assembly: AssemblyVersion("2.1.0")]
+[assembly: AssemblyInformationalVersion("2.1.0")]
+//DONT ADD: [assembly: AssemblyFileVersion("2.1.0")] !!!
